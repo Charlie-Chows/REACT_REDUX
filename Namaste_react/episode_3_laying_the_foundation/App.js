6827adde68838d440238fe799a00d605ffd73360
Charlie-Chows <sujithkasthuri247@gmail.com>
2023-11-20 20:35:50 +0530

    JSX added

--- Namaste_react/episode_3_laying_the_foundation/App.js
+++ Namaste_react/episode_3_laying_the_foundation/App.js
@@ -4,6 +4,7 @@ import  ReactDOM  from "react-dom/client";
 const reactHeading = React.createElement("h1",{id: "reactHeading"},"React Heading");
 const jsxHeading = <h1 id="JSXHeading">JSX Heading</h1>;
 
+// Both will console the object
 console.log(jsxHeading);
 console.log(reactHeading);
 
